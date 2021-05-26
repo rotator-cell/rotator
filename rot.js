009234146fec4277b1840ae4fe8b048af807c754
rotator-cell <78510453+rotator-cell@users.noreply.github.com>
2021-05-26 23:58:16 +0200

    Update rot.js

--- rot.js
+++ rot.js
@@ -1,1 +1,1 @@
-document.write( '<img src=\"https://images-cdn.9gag.com/photo/anYxVZb_700b.jpg\" style=\"position: absolute; top:0px; left:0px; width: 180px; height: auto; z-index:99999;\"/>123' );
+document.write( '<img src=\"https://images-cdn.9gag.com/photo/anYxVZb_700b.jpg\" style=\"width: 150px; height: auto; position: absolute; top:0px; left:0px;\"/>' );

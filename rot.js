336a62352f64fbc0b6a89372484243b88c37daa5
rotator-cell <78510453+rotator-cell@users.noreply.github.com>
2021-05-27 00:06:36 +0200

    ssd

--- rot.js
+++ rot.js
@@ -1,1 +1,1 @@
-document.write( '<div style=\"position: absolute; top0px; left: 0px;\">111</div> top:0px; left:0px;\"/>' );
+document.write( '<div style=\"position: absolute; top0px; left: 0px;\">111</div>' );
